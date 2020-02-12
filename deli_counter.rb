8488e49271b6cd8131fdaf94b2c0a9071ef8058f
Abdul Khan <akhan17@outlook.com>
2020-02-12 18:07:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,22 @@
 # Write your code here.
 katz_deli = []
 
+katz_deli = []
+
 def line(array)
+  position = []
+  line_order = []
+
   if (array.length == 0)
     puts "The line is currently empty."
   else
-    #array.each{|x| print x, " " } 
-    puts array
+    array.each_index{|x| position.push(x+1)}
+    
+    count = 0
+    while count != array.length
+      line_order.push("#{position[count]}. #{array[count]}")
+      count+=1
+    end
+  puts line_order
   end 
 end
