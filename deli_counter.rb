aefcee07b5d6c5cf151cf0d79608feb5df587151
Abdul Khan <akhan17@outlook.com>
2020-02-12 17:49:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,9 @@
 katz_deli = []
 
 def line(array)
-  if array.length == 0 
+  if (array.length == 0)
     puts "The line is currently empty."
-  end
+  else
+    array.each{|x| print x, " " } 
+  end 
 end
